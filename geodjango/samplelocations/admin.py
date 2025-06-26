ff027f19b17a4f5e462eedf55bc8e5945516a113
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2025-06-26 10:46:46 -0700

    register more models

--- geodjango/samplelocations/admin.py
+++ geodjango/samplelocations/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import SampleLocation, Owner, Contact
+from .models import SampleLocation, Owner, Contact, Well, Lexicon, WellScreen, Equipment, Spring 
 
 admin.site.register(SampleLocation)
 admin.site.register(Owner)
 admin.site.register(Contact)
+admin.site.register(Lexicon)
+admin.site.register(Well)
+admin.site.register(WellScreen)
+admin.site.register(Equipment)
+admin.site.register(Spring)
