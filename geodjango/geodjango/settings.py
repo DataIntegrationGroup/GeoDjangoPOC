08f9800bc155cc4af5a679ed0496e3f5fa7a7139
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2025-07-07 15:34:59 -0700

    Adds if else to check environment for env variables

--- geodjango/geodjango/settings.py
+++ geodjango/geodjango/settings.py
@@ -13,12 +13,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-# Load environment variables from .env file
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+local_env_path = BASE_DIR / '.env'
+
+if local_env_path.exists():
+    load_dotenv(local_env_path)
+
+elif os.getenv('GOOGLE_CLOUD_PROJECT'):
+    # app engine environment
+    pass
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
