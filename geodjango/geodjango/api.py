11f9664e6efb474ee2f1be77ab948e8c76c4deaa
Jacob Brown <brown.jacob.alexander@gmail.com>
2025-07-08 10:34:15 -0600

    fix: remove tailing endpoint slashes

--- geodjango/geodjango/api.py
+++ geodjango/geodjango/api.py
@@ -2,6 +2,6 @@ from ninja import NinjaAPI
 
 api = NinjaAPI()
 
-api.add_router('/locations/', 'samplelocations.api.locations.router')
-api.add_router('/wells/', 'samplelocations.api.wells.router')
-api.add_router('/contacts/', 'samplelocations.api.contacts.router')
+api.add_router('/locations', 'samplelocations.api.locations.router')
+api.add_router('/wells', 'samplelocations.api.wells.router')
+api.add_router('/contacts', 'samplelocations.api.contacts.router')
