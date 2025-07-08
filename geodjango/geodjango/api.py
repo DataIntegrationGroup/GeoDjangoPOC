e672a108bc91239d1d5919bf144b5d6033b4b23b
Jacob Brown <brown.jacob.alexander@gmail.com>
2025-07-08 10:12:33 -0600

    split routers into own files

--- geodjango/geodjango/api.py
+++ geodjango/geodjango/api.py
@@ -2,4 +2,6 @@ from ninja import NinjaAPI
 
 api = NinjaAPI()
 
-api.add_router('/samplelocations/', 'samplelocations.api.router')
+api.add_router('/locations/', 'samplelocations.api.locations.router')
+api.add_router('/wells/', 'samplelocations.api.wells.router')
+api.add_router('/contacts/', 'samplelocations.api.contacts.router')
---
+++ geodjango/samplelocations/api/locations.py
@@ -0,0 +1,38 @@
+from ninja import Router
+from ..models import SampleLocation, Well, WellScreen, Contact, Owner, Lexicon
+from django.contrib.gis.geos import Point
+from django.shortcuts import get_object_or_404
+
+router = Router()
+
+@router.get('')
+def get_locations(request):
+    """
+    List all sample locations.
+    """    
+    locations = SampleLocation.objects.all()
+    return [
+        {
+            'id': loc.id,
+            'name': loc.name,
+            'lat': loc.point.y,
+            'lon': loc.point.x,
+            'description': loc.description,
+            'visible': loc.visible,
+            'date_created': loc.date_created.isoformat(),
+        }
+        for loc in locations
+    ]
+
+@router.post("")
+def post_location(request, name: str, lat: float, lon: float, owner_id: int, description: str = None, visible: bool = False):
+    owner = get_object_or_404(Owner, id=owner_id)
+    point = Point(lon, lat)
+    location = SampleLocation.objects.create(
+        name=name,
+        description=description,
+        visible=visible,
+        point=point,
+        owner=owner
+    )
+    return {"id": location.id, "name": location.name}
