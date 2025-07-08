58a91631b456ad689108d017f4cee0569c73722b
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2025-07-07 23:40:36 -0700

    Adds prod database url handling in environ for connection

--- geodjango/geodjango/settings.py
+++ geodjango/geodjango/settings.py
@@ -17,6 +17,7 @@ from dotenv import load_dotenv, dotenv_values
 import os
 
 from google.cloud import secretmanager
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,16 +110,29 @@ WSGI_APPLICATION = 'geodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "5432"),
+if "DATABASE_URL" in os.environ:
+    #use prod url if available
+    DATABASES = {
+        'default': dj_database_url.parse(
+            os.environ['DATABASE_URL'],
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+            engine='django.contrib.gis.db.backends.postgis'
+        )
+    } 
+else:
+    # use local dev db
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv("DB_HOST", "localhost"),
+            'PORT': os.getenv("DB_PORT", "5432"),
+        }
     }
-}
 
 
 # Password validation
