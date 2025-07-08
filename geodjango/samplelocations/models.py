4df95f8566c978803ba53debb90b2dc48000687c
ksmuczynski <ksmuczynski@gamil.com>
2025-07-08 14:23:56 -0600

    Update/add models based on MVP Dreama.

--- geodjango/samplelocations/models.py
+++ geodjango/samplelocations/models.py
@@ -1,82 +1,67 @@
 from django.contrib.gis.db import models
 
-#-------- Lexicon model -----------
-class Lexicon(models.Model):
+#--------Location model -----------
+
+class Location(models.Model):
+    location_id = models.BigAutoField(primary_key=True)
+    point = models.PointField(srid=4326, spatial_index=True, dim=3, db_comment= "Defines the (X,Y,Z) coordinates of a location") #Did we come to a consensus on srid? 'dim = 3' allows the point to store Z information, i.e. elevation information.
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Location"
+        verbose_name_plural = "Locations"
+        db_table_comment = "This table stores point locations on the earth's surface"
+
+#--------Thing model -----------
+class Thing(models.Model):
+    thing_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
-    description = models.CharField(max_length=255, blank=True, null=True)
+    release_status = models.BooleanField(default=False) #TODO: What information does this field capture?
     date_created = models.DateTimeField(auto_now_add=True)
+
+    # Define the M:M relationship using the 'through' option.
+    # verbose_name option is also required for ManyToManyField (https://docs.djangoproject.com/en/5.2/topics/db/models/#verbose-field-names)
+    location = models.ManyToManyField(Location, through="Location_Thing_Junction", verbose_name= "the related location")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Lexicon"
-        verbose_name_plural = "Lexicons"
-        ordering = ['name']
+        verbose_name = "Thing"
+        verbose_name_plural = "Things"
 
-#--------Sample Location base models -----------        
+#--------Junction model for the Location and Thing models -----------
+class Location_Thing_Junction(models.Model):
+    # Define 1:M relationship using models.ForeignKey
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name= "locations", verbose_name= "the related location") #Django appends "_id" to FK field names (https://docs.djangoproject.com/en/5.2/ref/models/fields/#database-representation)
+    thing = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name= "things", verbose_name= "the related thing")
 
-class SampleLocation(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=255, blank=True, null=True)
-    visible = models.BooleanField(default=False)
-    point = models.PointField(srid=4326, spatial_index=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey('Owner', related_name='samplelocations', on_delete=models.CASCADE)
-    #each sample location should have one owner, but an owner can have multiple sample locations
-    
-    def __str__(self):
-        return self.name
+    # Define composite primary key. A composite primary key, when defined using CompositePrimaryKey, is considered a virtual field.
+    pk = models.CompositePrimaryKey("location_id", "thing_id")
+
+    # Additional fields
+    effective_start = models.DateTimeField()
+    effective_end = models.DateTimeField()
 
     class Meta:
-        verbose_name = "Sample Location"
-        verbose_name_plural = "Sample Locations"
-        ordering = ['name']  # Order by name by default
+        db_table_comment = "Junction table linking Location and Thing"
 
-class Owner(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=255, blank=True, null=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    contact = models.ForeignKey('Contact', related_name='owners', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "Owner"
-        verbose_name_plural = "Owners"
-        ordering = ['name']
-
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20, blank=True, null=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "Contact"
-        verbose_name_plural = "Contacts"
-        ordering = ['name']
-
-class Well(models.Model):
-    location = models.ForeignKey(SampleLocation, related_name='wells', on_delete=models.CASCADE)
-    ose_pod_id = models.CharField(max_length=50, blank=True, null=True)
-    api_id = models.CharField(max_length=50, blank=True, default="")
-    usgs_id = models.CharField(max_length=50, blank=True, null=True)
+#--------WellThing model -----------
+class WellThing(models.Model):
+    wellthing_id = models.BigAutoField(primary_key=True)
+    thing = models.ForeignKey(Thing, related_name='wellthings', on_delete=models.CASCADE)
     well_depth = models.FloatField(blank=True, null=True, help_text="feet below ground surface")
     hole_depth = models.FloatField(blank=True, null=True, help_text="feet below ground surface")
-    well_type = models.ForeignKey(Lexicon, related_name='wells_by_type', on_delete=models.SET_NULL, blank=True, null=True)
     casing_diameter = models.FloatField(blank=True, null=True, help_text="inches")
     casing_depth = models.FloatField(blank=True, null=True, help_text="feet below ground surface")
     casing_description = models.CharField(max_length=50, blank=True, null=True)
-    construction_notes = models.CharField(max_length=250, blank=True, null=True)
-    formation_zone = models.ForeignKey(Lexicon, related_name='wells_by_formation', on_delete=models.SET_NULL, blank=True, null=True)
+    construction_notes = models.TextField(blank=True, null=True) # It is recommended to use TextField over CharField for text of variable and potentially large size
 
     def __str__(self):
-        return f"Well at {self.location.name}"
+        return f"Well at {self.location.point}"
 
 class WellScreen(models.Model):
     well = models.ForeignKey(Well, related_name='screens', on_delete=models.CASCADE)
