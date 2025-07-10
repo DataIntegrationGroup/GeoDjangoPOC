79cea33cc8efb933ac513dc18fd639a41b914254
ksmuczynski <ksmuczynski@gamil.com>
2025-07-10 16:03:37 -0600

    Import and register additional models

--- geodjango/samplelocations/admin.py
+++ geodjango/samplelocations/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Location, Thing, WellThing, Location_Thing_Junction
+from .models import Location, Thing, WellThing, SpringThing, Location_Thing_Junction, Sensor, Datastream, Observation, \
+    GroundwaterLevelObservation, Sample
+
 
 admin.site.register(Location)
 admin.site.register(Thing)
 admin.site.register(WellThing)
-#admin.site.register(Location_Thing_Junction)
+admin.site.register(SpringThing)
+admin.site.register(Location_Thing_Junction)
+admin.site.register(Sensor)
+admin.site.register(Datastream)
+admin.site.register(Observation)
+admin.site.register(GroundwaterLevelObservation)
+admin.site.register(Sample)
 #admin.site.register(Lexicon)
 #admin.site.register(WellScreen)
 #admin.site.register(Equipment)
