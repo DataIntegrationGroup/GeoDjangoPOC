e247269c26013923c7688ce7cf84dc7f6ec6ab13
ksmuczynski <ksmuczynski@gamil.com>
2025-07-10 15:16:40 -0600

    Update how the composite primary key is defined in the Location_Thing_Junction table. Documentation states models with composite primary keys cannot be registered with the admin.

--- geodjango/samplelocations/models.py
+++ geodjango/samplelocations/models.py
@@ -5,16 +5,15 @@ from django.contrib.gis.db import models
 class Location(models.Model):
     """Represents a point location on the earth's surface"""
     location_id = models.BigAutoField(primary_key=True)
-    point = models.PointField(
+    coordinate = models.PointField(
         srid=4326, #TODO: Did we come to a consensus on srid?
         spatial_index=True,
         dim=3, # Enable storage of Z data, e.g. elevation
-        db_comment= "Defines the (X,Y,Z) coordinates of a location" # Comment on the database field. Stored at the db level.
     )
     date_created= models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.location_id
 
     class Meta:
         verbose_name = "Location" # human-readable, singular name to the model. This name is used by Django in various contexts, most notably in the Django admin interface.
@@ -30,7 +29,7 @@ class Thing(models.Model):
     name = models.CharField(max_length=100, unique=True)
     release_status = models.BooleanField(default=False) #TODO: What information does this field capture?
     date_created = models.DateTimeField(auto_now_add=True)
-    # The 'location' field specifies the M:M relationship and specifies
+    # The 'location' field sets up the M:M relationship and specifies
     # the 'Locoation_Thing_Juncation' as the intermediate table.
     location = models.ManyToManyField(
         Location,
@@ -47,30 +46,25 @@ class Thing(models.Model):
         verbose_name_plural = "Things"
 
 
-#--------Junction model for the Location and Thing models -----------
+#--------Junction model linking the Location and Thing models -----------
 
 class Location_Thing_Junction(models.Model):
     """A junction model linking a Thing to a Location"""
-    # Define a 1:M relationship using models.ForeignKey.
+    # Define a 1:M relationship using models.ForeignKey().
     # Django appends "_id" to FK field names (https://docs.djangoproject.com/en/5.2/ref/models/fields/#database-representation)
     location = models.ForeignKey(
         Location,
         on_delete=models.CASCADE,
-        related_name= "locations",
+        related_name= "locations-things",
         verbose_name= "related location"
     )
     thing = models.ForeignKey(
         Thing,
         on_delete=models.CASCADE,
-        related_name= "things",
+        related_name= "locations-things",
         verbose_name= "related thing"
     )
-
-    # Define composite primary key. A composite primary key, when defined using
-    # CompositePrimaryKey, is considered a virtual field.
-    pk = models.CompositePrimaryKey("location_id", "thing_id")
-
-    # Additional fields
+      # Additional fields
     effective_start = models.DateTimeField()
     effective_end = models.DateTimeField()
 
@@ -78,49 +72,70 @@ class Location_Thing_Junction(models.Model):
         return f"{self.location} - {self.thing}"
 
     class Meta:
+        # This enforces that the combination of 'location' and 'thing' must be unique. This acts as a composite
+        # primary key for the purpose of uniqueness, while still using the default 'id' field as the actual primary
+        # key, which is necessary for the Django admin to work correctly.
+        constraints = [
+            models.UniqueConstraint(fields=["location", "thing"], name="unique_location-thing")
+        ]
         db_table_comment = "Junction table linking Location and Thing models"
 
 
 #--------WellThing model. Inherits all fields from Thing model -----------
 class WellThing(Thing):
-    """A specific type of monitoring station (Thing)."""
+    """ A specific type of monitoring station (Thing)."""
     # This field creates the inheritance link from WellThing back to Thing.
-    thing_ptr= models.OneToOneField(Thing, parent_link = True, related_name='things', verbose_name="related thing", on_delete=models.CASCADE)
+    # The name 'thing_ptr' is a conventional naming choice in Django for the parent link field,
+    thing_ptr= models.OneToOneField(
+        Thing,
+        on_delete=models.CASCADE,
+        parent_link = True,
+        related_name='wellthings',
+        verbose_name="related thing"
+    )
     well_depth = models.FloatField(blank=True, null=True, help_text="feet below ground surface")
     hole_depth = models.FloatField(blank=True, null=True, help_text="feet below ground surface")
     casing_diameter = models.FloatField(blank=True, null=True, help_text="inches")
     casing_depth = models.FloatField(blank=True, null=True, help_text="feet below ground surface")
     casing_description = models.CharField(max_length=50, blank=True, null=True)
-    construction_notes = models.TextField(blank=True, null=True) # It is recommended to use TextField over CharField for text of variable and potentially large size
+    construction_notes = models.TextField(blank=True, null=True) # Use TextField over CharField for long-form text of variable length without a predefined limit.
 
     def __str__(self):
-        return f"Well at {self.location.point}"
+        return f"{self.name} (Well)"
 
-class WellScreen(models.Model):
-    well = models.ForeignKey(Well, related_name='screens', on_delete=models.CASCADE)
-    screen_depth_top = models.FloatField(help_text="feet below ground surface")
-    screen_depth_bottom = models.FloatField(help_text="feet below ground surface")
-    screen_type = models.ForeignKey(Lexicon, related_name='well_screens_by_type', on_delete=models.SET_NULL, blank=True, null=True)
+#--------Datastream (Series) model-----------
+class Datastream(models.Model):
+    """ A collection of observations from a specific Thing"""
+    thing = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name="datastreams", verbose_name="related thing")
+    observed_property = models.TextField(blank=True, null=True)
+    sensor_id
+    release_status
 
-    def __str__(self):
-        return f"Screen {self.screen_depth_top}-{self.screen_depth_bottom} ft for {self.well}"
+# class WellScreen(models.Model):
+#     well = models.ForeignKey(Well, related_name='screens', on_delete=models.CASCADE)
+#     screen_depth_top = models.FloatField(help_text="feet below ground surface")
+#     screen_depth_bottom = models.FloatField(help_text="feet below ground surface")
+#     screen_type = models.ForeignKey(Lexicon, related_name='well_screens_by_type', on_delete=models.SET_NULL, blank=True, null=True)
+#
+#     def __str__(self):
+#         return f"Screen {self.screen_depth_top}-{self.screen_depth_bottom} ft for {self.well}"
 
-class Equipment(models.Model):
-    equipment_type = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-    serial_no = models.CharField(max_length=50)
-    date_installed = models.DateTimeField(blank=True, null=True)
-    date_removed = models.DateTimeField(blank=True, null=True)
-    recording_interval = models.IntegerField(blank=True, null=True)
-    equipment_notes = models.CharField(max_length=50, blank=True, null=True)
-    location = models.ForeignKey(SampleLocation, related_name='equipment', on_delete=models.CASCADE)
+# class Equipment(models.Model):
+#     equipment_type = models.CharField(max_length=50)
+#     model = models.CharField(max_length=50)
+#     serial_no = models.CharField(max_length=50)
+#     date_installed = models.DateTimeField(blank=True, null=True)
+#     date_removed = models.DateTimeField(blank=True, null=True)
+#     recording_interval = models.IntegerField(blank=True, null=True)
+#     equipment_notes = models.CharField(max_length=50, blank=True, null=True)
+#     location = models.ForeignKey(SampleLocation, related_name='equipment', on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return f"{self.equipment_type} ({self.serial_no}) at {self.location.name}"
 
-    def __str__(self):
-        return f"{self.equipment_type} ({self.serial_no}) at {self.location.name}"
-
-class Spring(models.Model):
-    description = models.CharField(max_length=255, blank=True, null=True)
-    location = models.ForeignKey(SampleLocation, related_name='springs', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"Spring at {self.location.name}"
+# class Spring(models.Model):
+#     description = models.CharField(max_length=255, blank=True, null=True)
+#     location = models.ForeignKey(SampleLocation, related_name='springs', on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return f"Spring at {self.location.name}"
