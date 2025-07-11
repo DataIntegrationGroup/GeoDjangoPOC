c33216472a35c68ab98ed9835aa6d1dc56404120
ksmuczynski <ksmuczynski@gamil.com>
2025-07-10 21:37:06 -0600

    Created SpringThing, Sensor, DataStream, Sample, Observation, and GroundwaterLevelObservation models

--- geodjango/samplelocations/models.py
+++ geodjango/samplelocations/models.py
@@ -2,11 +2,15 @@ from django.contrib.gis.db import models
 
 #--------Location model -----------
 
+#TODO: Additional admin configuration is needed for this model. The admin panel currently
+# displays a text box for the coordinate field and it is unclear what format is expected.
+# I've read a few things about widgets being helpful here.
+
 class Location(models.Model):
     """Represents a point location on the earth's surface"""
     location_id = models.BigAutoField(primary_key=True)
     coordinate = models.PointField(
-        srid=4326, #TODO: Did we come to a consensus on srid?
+        srid=4326,
         spatial_index=True,
         dim=3, # Enable storage of Z data, e.g. elevation
     )
@@ -27,7 +31,7 @@ class Thing(models.Model):
     """A base model representing a generic monitoring station (Thing)"""
     thing_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
-    release_status = models.BooleanField(default=False) #TODO: What information does this field capture?
+    release_status = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
     # The 'location' field sets up the M:M relationship and specifies
     # the 'Locoation_Thing_Juncation' as the intermediate table.
@@ -55,13 +59,13 @@ class Location_Thing_Junction(models.Model):
     location = models.ForeignKey(
         Location,
         on_delete=models.CASCADE,
-        related_name= "locations-things",
+        related_name= "locations_things",
         verbose_name= "related location"
     )
     thing = models.ForeignKey(
         Thing,
         on_delete=models.CASCADE,
-        related_name= "locations-things",
+        related_name= "locations_things",
         verbose_name= "related thing"
     )
       # Additional fields
@@ -76,14 +80,16 @@ class Location_Thing_Junction(models.Model):
         # primary key for the purpose of uniqueness, while still using the default 'id' field as the actual primary
         # key, which is necessary for the Django admin to work correctly.
         constraints = [
-            models.UniqueConstraint(fields=["location", "thing"], name="unique_location-thing")
+            models.UniqueConstraint(fields=["location", "thing"], name="unique_location_thing")
         ]
         db_table_comment = "Junction table linking Location and Thing models"
 
 
 #--------WellThing model. Inherits all fields from Thing model -----------
+
 class WellThing(Thing):
-    """ A specific type of monitoring station (Thing)."""
+    """ A specific type of monitoring station (Thing) representing a well."""
+    wellthing_id = models.BigAutoField(primary_key=True)
     # This field creates the inheritance link from WellThing back to Thing.
     # The name 'thing_ptr' is a conventional naming choice in Django for the parent link field,
     thing_ptr= models.OneToOneField(
@@ -103,39 +109,92 @@ class WellThing(Thing):
     def __str__(self):
         return f"{self.name} (Well)"
 
+
+#--------SpringThing model. Inherits all fields from Thing model -----------
+
+class SpringThing(Thing):
+    """ A specific type of monitoring station (Thing) representing a spring."""
+    springthing_id = models.BigAutoField(primary_key=True)
+    # This field creates the inheritance link from SpringThing back to Thing.
+    # The name 'thing_ptr' is a conventional naming choice in Django for the parent link field,
+    thing_ptr = models.OneToOneField(
+        Thing,
+        on_delete=models.CASCADE,
+        parent_link=True,
+        related_name='springthings',
+        verbose_name="related thing"
+    )
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} (Spring)"
+
+
+#--------Sensor model-----------
+
+class Sensor(models.Model):
+    sensor_id = models.BigAutoField(primary_key=True)
+    serial_number = models.CharField(max_length=50, blank=True, null=True)
+    install_date = models.DateTimeField(blank=True, null=True)
+    model = models.CharField(max_length=50, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Sensor {self.serial_number} ({self.model})"
+
+
 #--------Datastream (Series) model-----------
+
 class Datastream(models.Model):
-    """ A collection of observations from a specific Thing"""
+    """ A collection of observations from a Thing collected using a Sensor."""
+    datastream_id = models.BigAutoField(primary_key=True)
     thing = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name="datastreams", verbose_name="related thing")
-    observed_property = models.TextField(blank=True, null=True)
-    sensor_id
-    release_status
+    sensor_id = models.OneToOneField(Sensor, on_delete=models.CASCADE, related_name="datastreams", verbose_name="related sensor")
+    observed_property = models.CharField(max_length=100)
+    release_status = models.BooleanField(default=False)
 
-# class WellScreen(models.Model):
-#     well = models.ForeignKey(Well, related_name='screens', on_delete=models.CASCADE)
-#     screen_depth_top = models.FloatField(help_text="feet below ground surface")
-#     screen_depth_bottom = models.FloatField(help_text="feet below ground surface")
-#     screen_type = models.ForeignKey(Lexicon, related_name='well_screens_by_type', on_delete=models.SET_NULL, blank=True, null=True)
-#
-#     def __str__(self):
-#         return f"Screen {self.screen_depth_top}-{self.screen_depth_bottom} ft for {self.well}"
 
-# class Equipment(models.Model):
-#     equipment_type = models.CharField(max_length=50)
-#     model = models.CharField(max_length=50)
-#     serial_no = models.CharField(max_length=50)
-#     date_installed = models.DateTimeField(blank=True, null=True)
-#     date_removed = models.DateTimeField(blank=True, null=True)
-#     recording_interval = models.IntegerField(blank=True, null=True)
-#     equipment_notes = models.CharField(max_length=50, blank=True, null=True)
-#     location = models.ForeignKey(SampleLocation, related_name='equipment', on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return f"{self.equipment_type} ({self.serial_no}) at {self.location.name}"
+#--------Sample model-----------
 
-# class Spring(models.Model):
-#     description = models.CharField(max_length=255, blank=True, null=True)
-#     location = models.ForeignKey(SampleLocation, related_name='springs', on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return f"Spring at {self.location.name}"
+class Sample(models.Model):
+    """Represents a sample collected from a Thing"""
+    sample_id = models.BigAutoField(primary_key=True)
+    thing = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name="samples", verbose_name="related thing")
+    sample_date = models.DateTimeField()
+    sample_notes = models.TextField(blank=True, null=True)
+
+    #TODO: This could be updated in a way that doesn't return the sample id in the admin panel.
+    # It's confusing in the admin panel to see "Sample 3 from Thing1" when it's the first sample
+    # from Thing1 but the third sample in the sample table. If this doesn't makes sense, try
+    # creating three samples from two different Things from the admin panel.
+    def __str__(self):
+        return f"Sample {self.sample_id} from {self.thing}"
+
+
+#--------Observation model-----------
+
+class Observation(models.Model):
+    """Represents a single observation from a datastream?"""
+    observation_id = models.BigAutoField(primary_key=True)
+    sample = models.ForeignKey(Sample, on_delete=models.CASCADE, related_name="observations", verbose_name="related sample")
+    datastream = models.ForeignKey(Datastream, on_delete=models.CASCADE, related_name="observations", verbose_name="related datastream")
+    observed_value = models.FloatField(help_text="The value of the observation")
+    release_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Observation {self.observation_id} for {self.datastream}"
+
+#--------GroundwaterLevelObservation model-----------
+class GroundwaterLevelObservation(Observation):
+    """A specific type of observation (groundwater levels)"""
+    groundwater_level_observation_id = models.BigAutoField(primary_key=True)
+    observation_ptr = models.OneToOneField(
+        Observation,
+        on_delete=models.CASCADE,
+        parent_link=True,
+        related_name='groundwater_level_observations',
+        verbose_name="related observation"
+    )
+
+    def __str__(self):
+        return f"Groundwater Level Observation {self.observation_id} for {self.datastream}"
