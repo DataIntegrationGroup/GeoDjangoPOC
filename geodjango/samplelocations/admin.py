cdb0dbededbbc10edf52d9732d5adbf25b17a162
Jacob Brown <brown.jacob.alexander@gmail.com>
2025-07-11 12:48:40 -0600

    fix: add locations with coordinates and display with table fields

--- geodjango/samplelocations/admin.py
+++ geodjango/samplelocations/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from .models import Location, Thing, WellThing, SpringThing, Location_Thing_Junction, Sensor, Datastream, Observation, \
+from django import forms
+from django.contrib.gis.admin import GISModelAdmin
+from django.contrib.gis.geos import Point
+from samplelocations.models import Location, Thing, WellThing, SpringThing, Location_Thing_Junction, Sensor, Datastream, Observation, \
     GroundwaterLevelObservation, Sample
 
+class LocationForm(forms.ModelForm):
+    x = forms.FloatField(label="Longitude")
+    y = forms.FloatField(label="Latitude")
+    z = forms.FloatField(label="Elevation")
 
-admin.site.register(Location)
+
+    class Meta:
+        model = Location
+        fields = ["x", "y", "z"]
+
+    def save(self, commit=True):
+        self.instance.coordinate = Point(self.cleaned_data["x"], self.cleaned_data["y"], self.cleaned_data["z"], srid=4326)
+        return super().save(commit=commit)
+
+@admin.register(Location)
+class LocationAdmin(GISModelAdmin):
+    form = LocationForm
+    list_display = ("location_id", "coordinate", "date_created")
+
 admin.site.register(Thing)
 admin.site.register(WellThing)
 admin.site.register(SpringThing)
